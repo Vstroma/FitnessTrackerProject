445bbdb2268a85b9587dd896eb5ef6465927530c
Vincent <tuo46190@temple.edu>
2024-02-27 09:48:25 -0500

    localhost browser connectable

--- demo/src/main/java/com/example/demo/FitnessApplication.java
+++ demo/src/main/java/com/example/demo/FitnessApplication.java
@@ -3,11 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class DemoApplication {
+public class FitnessApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(FitnessApplication.class, args);
 		System.out.println("DemoApplication");
 	}
 
